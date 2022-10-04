cf770c9b5966d42e11a2505c0e3c33a0059cf06e
Abhishek <takawaneabhishek04@gmail.com>
2022-10-04 17:12:08 +0530

    first commit

---
+++ fi.js
@@ -0,0 +1,40 @@
+
+let l1 = 5; 
+let l2 = 6; 
+let l3 = 7; 
+let s = (l1 + l2 + l3)/2;
+let area =  Math.sqrt(s*((s-l1)*(s-l2)*(s-l3)));
+console.log("Area of side having length 5, 6 , 7 =  "+area)
+
+filename = "abc.java"
+console.log("The extension of file system.php = "+filename.split('.').pop());
+filename = "abhi.js"
+console.log("The extension of file abc.js = "+filename.split('.').pop());
+
+let a=5
+let b=10
+let c= a+b
+  console.log("Sum of Numbers  5 and 10 ="+ c);
+
+if (a==b){
+    let e=3
+    let d = c*e
+   console.log("The triple is"+d);
+}
+else{
+   console.log(c);
+}
+
+let m=10
+let n=10
+let o= m+n
+  console.log("Sum of 10 & 10 Numbers = "+ o);
+
+if (m==n){
+   
+    let f = o*3
+   console.log("The triple is = "+f);
+}
+else{
+   console.log(c);
+}
